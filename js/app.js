0f88603dc9c0821f445d04a5e08cbc59709e425f
Sean Murray <sean.j.murray1@gmail.com>
2020-05-04 16:42:27 -0700

    Add functions for fill

--- js/app.js
+++ js/app.js
@@ -15,19 +15,46 @@ function howManyCookies(randomNum,averageCookies){
 var test = [2,4,6,8,10,10,8,6,4,2];
 // Finds total cookies from array
 function totalCookies(cookieArray){
-  function adder(total,current){
-    return total + current;
-  }
+  var adder = (total,current) => total + current;
   return cookieArray.reduce(adder);
 }
-console.log(totalCookies(test));
+// Fills cookie array
+function fillCookies(hours,min,max,avg,array){
+  for(var i =0; i < hours; i++){
+    var cookieHour = howManyCookies(randomNumber(min,max),avg);
+    array.push(cookieHour);
+  }
+  array.push(totalCookies(array));
+}
+// Fills hours of op array
+function hoursOfOp(start,total,array){
+  for(var i=0;i<total;i++)
+    if(start < 12){
+      array.push(`${start}am`);
+      start++;
+    }else if(start === 12){
+      array.push(`${start}pm`);
+      start++;
+    }else{
+      array.push(`${start - 12}pm`);
+      start++;
+    }
+}
 
 var seattle = {
   minCust: 23,
   maxCust: 65,
+  start: 6,
+  end: 20,
+  hours: [],
   avgCookie: 6.3,
-
+  cookiesPerHour: [],
+  fill: function(){
+    fillCookies((this.end-this.start),this.minCust,this.maxCust,this.avgCookie,this.cookiesPerHour);
+    hoursOfOp(this.start,(this.end-this.start),this.hours);
+  }
 };
 
-
-
+seattle.fill();
+console.log(seattle.cookiesPerHour);
+console.log(seattle.hours);
